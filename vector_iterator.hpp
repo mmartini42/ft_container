e34986a43f06c6a5cd78201de40a534819fb5ec4
mmartini42 <mathmart@student.42lyon.fr>
2022-09-25 14:46:23 +0200

    Creation of iterator and reverse iterator

---
+++ vector_iterator.hpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2022 mmartini42 <mathmart@42lyon.fr>.
+
+//
+// Created by mathis martini on 22/09/2022.
+//
+
+#ifndef FT_CONTAINER_VECTOR_ITERATOR_CUH
+#define FT_CONTAINER_VECTOR_ITERATOR_CUH
+
+namespace ft {
+
+	class vector_iterator {
+
+	};
+
+} // ft
+
+#endif //FT_CONTAINER_VECTOR_ITERATOR_CUH
