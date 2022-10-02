5fd9274fe8c0133109d465ae7dc66cf2ad533ebf
mmartini42 <mathmart@student.42lyon.fr>
2022-10-02 02:32:55 +0200

    ADD const map iterator

--- incs/Map/iterator/iteratorMap.hpp
+++ incs/Map/iterator/iteratorMap.hpp
@@ -107,6 +107,106 @@ namespace ft {
 		return (x);
 	}
 
+	template<class T, class G>
+	class const_MapIterator {
+	public:
+		typedef ft::bidirectional_iterator_tag  iterator_category;
+		typedef T                               const_node_ptr;
+		typedef std::ptrdiff_t                  difference_type;
+		typedef G        	                  	value_type;
+		typedef const value_type&          	    reference;
+		typedef const value_type*          	    pointer;
+
+		typedef ft::mapIterator<typename ft::node<value_type>::node_pointer, value_type>
+				mapIt;
+
+	private:
+		 const_node_ptr _ptr;
+
+	public:
+		explicit const_MapIterator(const_node_ptr ptr = NULL) : _ptr(ptr) {}
+		const_MapIterator(const const_MapIterator<T,G>& ref) : _ptr(ref._ptr) {}
+		const_MapIterator(const mapIt& ref) : _ptr(ref.getNode()) {}
+		~const_MapIterator() {}
+
+		const_MapIterator	getNode() const { return _ptr; }
+
+		const_MapIterator&	operator++() {
+			_ptr = nextIter(_ptr);
+			return *this;
+		}
+
+		const_MapIterator&	operator--() {
+			_ptr = prevIter(_ptr);
+			return *this;
+		}
+
+		const_MapIterator	operator++(int) {
+			const_MapIterator	*tmp(*this);
+			++*(this);
+			return tmp;
+		}
+
+		const_MapIterator	operator--(int) {
+			const_MapIterator	*tmp(*this);
+			--*(this);
+			return tmp;
+		}
+
+	private:
+
+		bool		isLeftChild(const_node_ptr node) { return node == node->parent->left; }
+
+		const_node_ptr	nextIter(const_node_ptr node) {
+			if (!node->right->is_leaf)
+				return (minPos(node->right));
+			while (!isLeftChild(node))
+				node = node->parent;
+			return node->parent;
+		}
+
+		const_node_ptr	prevIter(const_node_ptr node) {
+			if (!node->left->is_leaf)
+				return (maxPos(node->left));
+			//auto in CXX11
+			const_node_ptr	new_node = static_cast<const_node_ptr>(node);
+
+			while (isLeftChild(new_node))
+				new_node = new_node->parent;
+			return new_node->parent;
+		}
+
+	};
+
+	template <class T, class G>
+	bool operator==(const const_MapIterator<T, G>& x, const const_MapIterator<T, G>& y) {
+		return (x.getNode() == y.getNode());
+	}
+
+	template <class T, class G>
+	bool operator!=(const const_MapIterator<T, G>& x, const const_MapIterator<T, G>& y) {
+		return !(x.getNode() == y.getNode());
+	}
+
+	template <class node_ptr, class const_node_ptr, class G>
+	bool operator==(const mapIterator<node_ptr, G>& x, const const_MapIterator<const_node_ptr, G>& y) {
+		return (x.getNode() == y.getNode());
+	}
+
+	template <class node_ptr, class const_node_ptr, class G>
+	bool operator!=(const mapIterator<node_ptr, G>& x, const const_MapIterator<const_node_ptr, G>& y) {
+		return (x.getNode() != y.getNode());
+	}
+
+	template <class node_ptr, class const_node_ptr, class G>
+	bool operator==(const const_MapIterator<const_node_ptr, G>& x, const mapIterator<node_ptr, G>& y) {
+		return (x.getNode() == y.getNode());
+	}
+
+	template <class node_ptr, class const_node_ptr, class G>
+	bool operator!=(const const_MapIterator<const_node_ptr, G>& x, const mapIterator<node_ptr, G>& y) {
+		return (x.getNode() != y.getNode());
+	}
 } // ft
 
 #endif //FT_CONTAINER_ITERATORMAP_HPP
